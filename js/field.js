caa60645b00d5be16865306d79f1b826498e3771
Vladislav Efanov <vladislav.efanov@gmail.com>
2018-11-13 13:01:32 +0400

    Форматировал код. Удалил ненужные картинки.

--- js/field.js
+++ js/field.js
@@ -1,13 +1,13 @@
-$(document).ready(function(){  
-    $('.col-auto').on('click', function() {
-        let selected = ($(this).hasClass('selected')) ? true : false;
+$(document).ready(function() {
+	$('.col-auto').on('click', function() {
+		let selected = ($(this).hasClass('selected')) ? true : false;
 
-        if (!selected) {
-            $(this).addClass('selected');
-        } else {
-            $(this).removeClass('selected');
-        }
-    });
+		if (!selected) {
+			$(this).addClass('selected');
+		} else {
+			$(this).removeClass('selected');
+		}
+	});
 
-    $('.disabled').off('click');
+	$('.disabled').off('click');
 });
